2579138b000517d8d872fe98054060ce8b51629f
Agilulfe <guillaume.jacobs1995@outlook.com>
2020-04-06 12:25:40 +0200

    Create portfolio website

---
+++ server.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Flask app for the portfolio website
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+import csv
+
+from flask import Flask, render_template, url_for, request, redirect
+
+
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong. Try again!'
+
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
